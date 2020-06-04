bb11eb070766a90c3a1640f9c70a083306748b16
Cosimo Argentino <cosimoargentino@gmail.com>
2020-06-04 12:52:59 +0200

    Spring Cloud configuration server for microservices example

---
+++ src/test/java/com/arcos/microservices/limits/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arcos.microservices.limits.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
